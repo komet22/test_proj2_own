b42bfa3be0a934718d3e44a3cc8de251925705fa
Paweł Jaruga <helferth95@gmail.com>
2016-12-05 18:52:41 +0100

    [IMPLEMENT] Wymaganie 5
    Dodano klasę Client.

---
+++ src/tdd/Client.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+/**
+ *
+ * @author Peasant
+ */
+public class Client {
+    String firstName;
+    String lastName;
+    String email;
+    double cash;
+    Purchase purchase;
+    
+    public Client(String fname, String lname, String email, double cash)
+    {
+        this.firstName = fname;
+        this.lastName = lname;
+        this.email = email;
+        this.cash = cash;
+        purchase = new Purchase();
+    }
+}
